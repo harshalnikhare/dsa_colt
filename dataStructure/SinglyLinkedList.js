b7131634413f3edddf45409088eff59ab358d2f3
harshaln369 <harshaln369@gmail.com>
2023-07-23 19:34:11 +0530

    done: singlylinkedlist

--- dataStructure/SinglyLinkedList.js
+++ dataStructure/SinglyLinkedList.js
@@ -16,6 +16,7 @@ class SinglyLinkedList {
     if (!this.head) {
       this.head = this.tail = newNode;
     } else {
+      console.log(this.head === this.tail);
       this.tail.next = newNode;
       this.tail = newNode;
     }
@@ -61,14 +62,88 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  get(index) {
+    if (index < 0 || index > this.length) return null;
+
+    let counter = 0,
+      current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+
+    return false;
+  }
+
+  insert(index, val) {
+    let newNode = new Node(val);
+
+    if (index < 0 || index > this.length) return false;
+
+    if (index === 0) return !!this.unshift(val);
+    if (index === this.length) return !!this.push(val);
+
+    let prevNode = this.get(index - 1);
+    let nextNode = prevNode.next;
+    prevNode.next = newNode;
+    newNode.next = nextNode;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index > this.length - 1) return undefined;
+
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    let prevNode = this.get(index - 1);
+    let removedNode = prevNode.next;
+    prevNode.next = removedNode.next;
+    this.length--;
+
+    return removedNode;
+  }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+
+    let prev = null,
+      next;
+
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();
 
 list.push(10);
-list.push(20);
-list.push(30);
-list.shift();
-list.unshift(40);
-
 console.log(list);
+list.push(20);
+console.log(list);
+list.push(30);
+console.log(list);
+// list.shift();
+// list.unshift(40);
+
+// console.log(list.reverse());
+// console.log(JSON.stringify(list, null, 2));
