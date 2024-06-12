7a85c92cc78f71d16fdda98500f0fdbbb33514cd
harshalnikhare <harshaln369@gmail.com>
2024-06-12 21:02:20 +0530

    s5 => sameFrequency

--- s5-problem-solving-patterns/freqCounters/sameFrequency.js
+++ s5-problem-solving-patterns/freqCounters/sameFrequency.js
@@ -1,4 +1,21 @@
-function sameFrequency(num1, num2) {}
+function sameFrequency(num1, num2) {
+  num1 = num1.toString();
+  num2 = num2.toString();
+
+  if (num1.length !== num2.length) return false;
+
+  const freqCounter1 = {};
+
+  for (let num of num1) {
+    freqCounter1[num] = (freqCounter1[num] || 0) + 1;
+  }
+
+  for (let num of num2) {
+    if (!freqCounter1[num]) return false;
+    else freqCounter1[num]--;
+  }
+  return true;
+}
 
 console.log(sameFrequency(182, 281)); // true
 console.log(sameFrequency(34, 14)); // false
