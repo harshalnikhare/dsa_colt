5d01b2729c844f43853592acdaa13bb9692a67a4
Harshal Nikhare <harshaln369@gmail.com>
2024-06-12 18:44:08 +0530

    inProg: sameFreq

---
+++ s5-problem-solving-patterns/freqCounters/sameFrequency.js
@@ -0,0 +1,6 @@
+function sameFrequency(num1, num2) {}
+
+console.log(sameFrequency(182, 281)); // true
+console.log(sameFrequency(34, 14)); // false
+console.log(sameFrequency(3589578, 5879385)); // true
+console.log(sameFrequency(22, 222)); // false
